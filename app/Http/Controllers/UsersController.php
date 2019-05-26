2cd55615b24d3dea73c5fb5a00d9d6bc49fcf29e
Borris96 <borrispoe96@gmail.com>
2019-05-26 09:46:08 +0000

    User page layout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class UsersController extends Controller
 {
     public function create(){
         return view('users.create');
     }
+// ******两种方法调用users.show
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
+    // public function show($id){
+    //     return view('users.show')->withUser(User::find($id));
+    // }
 }
